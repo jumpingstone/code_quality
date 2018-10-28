b3fd1c8031bc0186d09cb0c2ca8245fec8255a27
Wei Chen <weich07@sap.com>
2018-10-28 11:10:16 +0800

    init check in

---
+++ sonar_script.sh
@@ -0,0 +1,5 @@
+mvn sonar:sonar \
+  -Dsonar.projectKey=sonar_test_in_jumpingstone_repo \
+  -Dsonar.organization=jumpingstone-github \
+  -Dsonar.host.url=https://sonarcloud.io \
+  -Dsonar.login=55bab0d33a2de1c23e9102ff2efb55be881eb54d
