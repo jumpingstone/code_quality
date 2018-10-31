0b18d369f951756c5c8f42743ca980120820a500
Wei Chen <weich07@sap.com>
2018-10-31 18:09:08 +0800

    add projects db

---
+++ services/src/main/java/com/jumpingstone/codequality/fireeye/service/Application.java
@@ -0,0 +1,15 @@
+package com.jumpingstone.codequality.fireeye.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.reactive.config.EnableWebFlux;
+
+@SpringBootApplication
+@EnableWebFlux
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
